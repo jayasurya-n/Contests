343e738a7ce1acfac355ece537b3ed0ed5b47aff
Jayasurya <jaya.svn@iitgn.ac.in>
2025-07-06 20:29:24 +0530

    cf-1028 div-2 done

--- CodeForces_Contest/CodeForces_Round_1028/q4.py
+++ CodeForces_Contest/CodeForces_Round_1028/q4.py
@@ -10,8 +10,34 @@ lii = lambda: list(map(int,input().strip().split()))
 
 class Solution:
     def run(self):
-        n = ii()
-        arr = lii()
+        n,q = lii()
+        b = lii()
+        queries = []
+        for _ in range(q):
+            x,y,z = lii()
+            queries.append((x-1,y-1,z-1))
+        
+        temp = b.copy()
+
+        for i in range(q-1,-1,-1):
+            x,y,z = queries[i]
+            val = temp[z]
+            temp[z] = -1
+            temp[x] = max(temp[x],val)
+            temp[y] = max(temp[y],val)
+        
+        for i in range(n):
+            if(temp[i]==-1):temp[i] = b[i]
+
+        a = temp.copy()
+
+        for i in range(q):
+            x,y,z = queries[i]
+            temp[z] = min(temp[x],temp[y])
+        
+        if(temp==b):return a
+        return [-1]
+
 
 if __name__ == "__main__":
     # fac = factorial(n=2*(10**5)+5,mod=10**9+7)
@@ -19,4 +45,4 @@ if __name__ == "__main__":
     # seed = random.randint(0,10**9+7)
     for _ in range(ii()):
         ans = Solution().run()
-        print(ans)
+        print(*ans)
