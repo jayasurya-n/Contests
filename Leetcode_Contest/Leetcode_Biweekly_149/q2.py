7a3ef8f211c8ff7c9a307bda052a61b8c272bbe5
Jayasurya <jaya.svn@iitgn.ac.in>
2025-07-09 18:12:56 +0530

    leetcode biweekly-149 done

--- Leetcode_Contest/Leetcode_Biweekly_149/q2.py
+++ Leetcode_Contest/Leetcode_Biweekly_149/q2.py
@@ -2,33 +2,30 @@ from typing import List,Optional
 from collections import deque, defaultdict
 import sys, math, heapq, bisect
 
+si = lambda: input().strip()
+ii = lambda: int(si())
+lsi = lambda: list(input().strip().split())
+lii = lambda: list(map(int,input().strip().split()))
+
 class Solution:
     def maxFreeTime(self, eventTime: int, k: int, startTime: List[int], endTime: List[int]) -> int:
         n = len(startTime)
-        
-        max_freetime = max(startTime[0],eventTime-endTime[-1])
+        gaps = [startTime[0]]
         for i in range(n-1):
-            max_freetime = max(max_freetime,startTime[i+1]-endTime[i])
-        
-        times = [endTime[i]-startTime[i] for i in range(n)]
-        for i in range(1,n):times[i]+=times[i-1]
+            gaps.append(startTime[i+1]-endTime[i])
+        gaps.append(eventTime-endTime[n-1])
 
-        i=j=0
-        while(j<n):
-            while(j-i+1>k):i+=1
-            free_time = startTime[j+1] if j+1<n else eventTime
-            free_time-= endTime[i-1] if i>0 else 0
-            free_time-=times[j]-(times[i-1] if i>0 else 0)
-            max_freetime = max(max_freetime,free_time)            
-            j+=1
-        return max_freetime
+        ans = csum = sum(gaps[:k+1])
+        print(ans)
+        for i in range(k+1,n+1):
+            csum+=gaps[i]-gaps[i-k-1]
+            ans = max(ans,csum)
+        return ans
 
-# time complexity: O(n)
+# time complexity: O(n+k)
 # space complexity: O(n)
 if __name__ == "__main__":
-    for _ in range(int(input().strip())):
-        eventTime = int(input().strip())        
-        k = int(input().strip())
-        startTime = list(map(int,input().strip().split()))
-        endTime = list(map(int,input().strip().split()))
-        print(Solution().maxFreeTime(eventTime,k,startTime,endTime))
+    for _ in range(ii()):
+        n = ii()
+        arr = lii()
+        print(Solution().func(arr,n))
--- Leetcode_Contest/Leetcode_Biweekly_149/q4.py
+++ Leetcode_Contest/Leetcode_Biweekly_149/q4.py
@@ -1,47 +1,47 @@
 from typing import List,Optional
 from collections import deque, defaultdict
-import sys, heapq, bisect
-from math import ceil, floor, gcd, sqrt, log
+import sys, math, heapq, bisect
 
 si = lambda: input().strip()
-ii = lambda: int(si)
+ii = lambda: int(si())
 lsi = lambda: list(input().strip().split())
 lii = lambda: list(map(int,input().strip().split()))
 
-def factorial(n,mod):
-    fac = [1]*n
-    for i in range(1,n):
-        fac[i] = (fac[i-1]*i)%mod
-    return fac
-
-def nCr(n,r,fac,mod):
-    if(n<r or n<0 or r<0):return 0
-    return fac[n]*pow(fac[r],mod-2,mod)*pow(fac[n-r],mod-2,mod)
-
-def prefixSum1D(arr,n):
-    prefix = [0]*n
-    prefix[0] = arr[0]
-    for i in range(1,n):prefix[i] = prefix[i-1]+arr[i]
-    return prefix
-
-def prefixSum2D(arr,m,n):
-    prefix = [[0]*n for _ in range(m)]
-    for i in range(m):
-        for j in range(n):
-            prefix[i][j] = arr[i][j]
-            if i>0:prefix[i][j] += prefix[i-1][j]
-            if j>0:prefix[a][b] += prefix[i][j-1]
-            if i>0 and j>0:prefix[i][j] -= prefix[i-1][j-1]
-    return prefix
-
 class Solution:
-    def run(self):
-        n = ii()
-        arr = lii()
+    def minCostGoodCaption(self, caption: str) -> str:
+        n = len(caption)
+        if(n<=2):return ""
 
+        # min cost to make good strings till i
+        dp = [[10**7]*26 for _ in range(n+1)]
+        for j in range(26):dp[0][j] = 0
+
+        best = [[""]*26 for _ in range(n+1)]
+
+        for i in range(3,n+1):
+            for j in range(26):
+                candidate = best[i-1][j]+chr(97+j)
+                if(dp[i][j]> dp[i-1][j]+abs(ord(caption[i-1])-(97+j))):
+                    dp[i][j] = dp[i-1][j]+abs(ord(caption[i-1])-(97+j))
+                    best[i][j] = candidate
+
+            mini = min(dp[i-3])
+            ks   = [k for k in range(26) if dp[i-3][k]==mini]
+            for j in range(26):
+                candidate = min([best[i-3][k]+chr(97+j)*3 for k in ks])
+                ope = 0
+                for k in range(3):ope+=abs(ord(caption[i-1-k])-(97+j))
+                if((dp[i][j]>mini+ope) or 
+                    (dp[i][j]==mini+ope and candidate<best[i][j])):
+                    dp[i][j] = mini+ope
+                    best[i][j] = candidate
+        
+        mini = min(dp[n])
+        return min(best[n][j] for j in range(26) if dp[n][j]==mini)
+
+# time complexity: O(26n)
+# space complexity: O(26n^2)
 if __name__ == "__main__":
-    # fac = factorial(n=2*(10**5)+5,mod=10**9+7)
-    # yes,no = "YES","NO"
-    for _ in range(int(input().strip())):
-        ans = Solution().run()
-        print(ans)
+    for _ in range(ii()):
+        caption = si()
+        print(Solution().minCostGoodCaption(caption))
